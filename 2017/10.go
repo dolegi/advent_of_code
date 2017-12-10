7a7932c7b905ae7aee43bb2a5086c77b69feaf42
Dominic Ginger <domginger@hotmail.co.uk>
2017-12-10 16:25:12 +0000

    Day 10 part 2

--- 2017/10.go
+++ 2017/10.go
@@ -15,23 +15,57 @@ func getItem(list []int, i int) int {
 	return list[i%len(list)]
 }
 
-func main() {
-	list := buildList(256)
-	fmt.Println(list)
-	lengths := []int{130,126,1,11,140,2,255,207,18,254,246,164,29,104,0,224}
+func getLengthsFromAscii(input string) (output []int) {
+	for _, c := range input {
+		output = append(output, int(c))
+	}
+	return
+}
+
+func calculateSparseHash(list []int, lengths []int) []int {
 	currentPosition := 0
 	skipSize := 0
-
-	for _, length := range lengths {
-		newList := make([]int, len(list))
-		copy(newList, list)
-		for i := currentPosition; i < currentPosition + length; i++ {
-			newList[(length + currentPosition - i + currentPosition - 1)%len(newList)] = getItem(list, i)
+	sparseHash := make([]int, len(list))
+	for j := 0; j < 64; j++ {
+		for _, length := range lengths {
+			copy(sparseHash, list)
+			for i := currentPosition; i < currentPosition+length; i++ {
+				sparseHash[(length+currentPosition-i+currentPosition-1)%len(sparseHash)] = getItem(list, i)
+			}
+			currentPosition += length + skipSize
+			skipSize++
+			copy(list, sparseHash)
 		}
-		currentPosition += length + skipSize
-		skipSize++
-		copy(list, newList)
-
-		fmt.Println(newList)
 	}
+	return sparseHash
+}
+
+func calculateDenseHash(sparseHash []int) []int {
+	xor := 0
+	denseHash := make([]int, 0, 16)
+	for i := 1; i <= len(sparseHash); i++ {
+		xor ^= sparseHash[i-1]
+		if i % 16 == 0 {
+			denseHash = append(denseHash, xor)
+			xor = 0
+		}
+	}
+	return denseHash
+}
+
+func hexEncodeArray(arr []int) (output string) {
+	for _, n := range arr {
+		output += fmt.Sprintf("%02x", n)
+	}
+	return
+}
+
+func main() {
+	list := buildList(256)
+	input := "130,126,1,11,140,2,255,207,18,254,246,164,29,104,0,224"
+	lengths := append(getLengthsFromAscii(input), 17, 31, 73, 47, 23)
+	sparseHash := calculateSparseHash(list, lengths)
+	denseHash := calculateDenseHash(sparseHash)
+	knotHash := hexEncodeArray(denseHash)
+	fmt.Println(knotHash)
 }
