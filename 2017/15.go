84edffb40528b1a17e772c69c633e6ec22eaea87
Dominic Ginger <domginger@hotmail.co.uk>
2017-12-16 13:17:45 +0000

    Day 15 part 2

--- 2017/15.go
+++ 2017/15.go
@@ -7,18 +7,23 @@ import (
 type generator struct {
 	factor int
 	currentValue int
+	criteria int
 }
 
 func (g *generator) nextValue() int {
 	g.currentValue = (g.currentValue * g.factor) % 2147483647
-	return g.currentValue
+	if g.currentValue % g.criteria == 0 {
+		return g.currentValue
+	} else {
+		return g.nextValue()
+	}
 }
 
 func main() {
-	generatorA := &generator{16807, 873}
-	generatorB := &generator{48271, 583}
+	generatorA := &generator{16807, 873, 4}
+	generatorB := &generator{48271, 583, 8}
 	count := 0
-	for i := 0; i < 40000000; i++ {
+	for i := 0; i < 5000000; i++ {
 		a := fmt.Sprintf("%032b\n", generatorA.nextValue())
 		b := fmt.Sprintf("%032b\n", generatorB.nextValue())
 
