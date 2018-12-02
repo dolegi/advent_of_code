62f96f1c7ed506889cec5e84af9062f4c4f8d3a3
Dominic Ginger <5877689+DominicGinger@users.noreply.github.com>
2018-12-02 23:22:18 +0100

    Day 1 and 2 for 2018

---
+++ 2018/2.js
@@ -0,0 +1,295 @@
+const input = `wxlnjevbfozadyiqpuzkrhstkg
+wxlsjivbfodamyiqpuzcxhstkg
+wxlnjevbfouammnqpuzcrhstkg
+wxlnjevbfobwmyiqpuzprhstkg
+wxlnjefbfodamyiqpuzcnustkg
+wxlnjevbfodakyyupuzcrhstkg
+wxlnjejbfodafynqpuzcrhstkg
+wxlnjevbfodomyifptzcrhstkg
+wxlnjevbfodamyiwcuzcrhstkz
+wxlnjevbfofamyiqpuznrhstxg
+wxlnjevbfodamyiqbupcrxstkg
+wxjnjevbfodxmyeqpuzcrhstkg
+xwlnjevbfosamyiqpuzcrhstkg
+fxlnjevbfodrmyiqpuzcrbstkg
+wxlnjevpfodamyiqquzzrhstkg
+wwlnjenbfodawyiqpuzcrhstkg
+wxrnjevbfodamyiqpuzlrhstrg
+welnjeqbfodkmyiqpuzcrhstkg
+walnjfvbfodamyiqpuzcrhwtkg
+wdlnjevbfodamriqpuzjrhstkg
+wxlnjevbfodmnyizpuzcrhstkg
+wxlnjevbfodgmyiqpuxcrjstkg
+wxlnjevbfkdamyiqpudcrestkg
+wxlyjevbfodamyiqpuzcehstkj
+wxlnjevamodamyiqpuzcrhatkg
+fxlnqevsfodamyiqpuzcrhstkg
+wqlnjevbfodanyiqvuzcrhstkg
+wxlnjevbfoiamyzqpuzcrnstkg
+wxlnjevbfodamyiqpuacrhsjig
+wxlnjuvbfodzmyvqpuzcrhstkg
+kxlnjevbfolamviqpuzcrhstkg
+wxlnjesbfldamyiqpuycrhstkg
+nxltjevbfodgmyiqpuzcrhstkg
+ojlnjevbfooamyiqpuzcrhstkg
+wxlnjevbfodaffiqpugcrhstkg
+cxlnievbfodamyuqpuzcrhstkg
+wxlouevbjodamyiqpuzcrhstkg
+wafnjevbfhdamyiqpuzcrhstkg
+wxlnjevbfxdamrpqpuzcrhstkg
+wxlnjepffodamyiqphzcrhstkg
+wxlnkevbfohamciqpuzcrhstkg
+wzlnmevdfodamyiqpuzcrhstkg
+wxlzjtvbfodamyiqpuzcrhstkd
+gxlnjehbfojamyiqpuzcrhstkg
+wxlnjeoqfodamyiqprzcrhstkg
+nxllvevbfodamyiqpuzcrhstkg
+wxlnjevtfomamyiqpurcrhstkg
+sxlnjevafodamyikpuzcrhstkg
+wxlnjevbfodamyfqpuzcyhztkg
+wxlnjevbfodamyiqpulnrhstkh
+wxlnwevbfodumyiqpuzqrhstkg
+wxldjevbfodamyiqpzzcrhstkk
+jxlnjevbfodamyiqphzcrnstkg
+fxlnjeibfodcmyiqpuzcrhstkg
+wxlnjevufodamyiqpnzcrhstkk
+wglnjevbfodamyiqpuzcesstkg
+wxlvjevbdodamyiqpuzcrhstkc
+wxlnjevbfodabyicpuzcrhstkl
+wxlnjevbfodamyiqpizcrhstvt
+wolnjevbfodawyiqiuzcrhstkg
+wxlyjevbfodamyuqpxzcrhstkg
+wxlijevbfodamyikpuzyrhstkg
+wxennevbfodamyiqpuzcrtstkg
+wxlnjevbyodamyuqpwzcrhstkg
+wxlnjevbfoiomyiqpuzcrhsteg
+wxlnjehbrodamyiqpuicrhstkg
+xxlnjevufodamyiqbuzcrhstkg
+wxlojevbfodamyiqpezcrhatkg
+wxljjevbfolamuiqpuzcrhstkg
+wxlnjevbfodamyiqruzcrhstpi
+wxlnjevbfomamyiqjnzcrhstkg
+wxlnjevbfodahyiqzuzcrhstpg
+wxtnjevbfodamyiqpuzcrhsdrg
+wxlnjevbfodamynrpuzcrhstkz
+wxlqjevefqdamyiqpuzcrhstkg
+wxlnjevbfmdamyiqnuzckhstkg
+wxlnjevbfodajyiqprzcrjstkg
+wxlnjqvbhodamyidpuzcrhstkg
+wxlnjhvbfodamriqpuzcchstkg
+wglnjevbfodamyiqpupfrhstkg
+wulnjevdfodamyiqpuzcrhsteg
+vxlojevbfodamyiqpuzcrhstsg
+wxlnjvvbfodamiiqpuzcrhttkg
+wxlnjevbfodabyiqpuzzrhetkg
+wxhnjevbfodamyiqpuwcrsstkg
+wslzjbvbfodamyiqpuzcrhstkg
+rxlnjevbfodhmyiqpupcrhstkg
+wxlnjevbfhdamyiqpuvcrhskkg
+wxlrjevbxodamyiqpuzcrhstag
+wxlsbevbfodammiqpuzcrhstkg
+wxlnjzvbfodemyiqpmzcrhstkg
+wxlnoevbfodgmyiqpuzbrhstkg
+wxlnjefbfodamyinpuzcrhwtkg
+bxlnjevbfwdamyiqpuocrhstkg
+cxlnjevbjodamyiqpuzcrhslkg
+wflnjevbforemyiqpuzcrhstkg
+wxlmjeoboodamyiqpuzcrhstkg
+wxlnjevbfadaiyiqpuzcrhutkg
+wxlnmevbfodamyyqpuzcrjstkg
+wxlnjovbfodamyippjzcrhstkg
+wxlnjmvbfodamyiqpszcrhsbkg
+wxlnjeebfodamyicpuxcrhstkg
+wxlnrehbfodamyiqpuzcrhytkg
+wxlnjevbfogamyiqwurcrhstkg
+wxlujevbnodamyiqpuzcrhstng
+wxlnoenofodamyiqpuzcrhstkg
+wxsnjevbfsdamyiqsuzcrhstkg
+wxlnjevwfodamyiqpuzxrhqtkg
+wxlnjevbnodamyiqxulcrhstkg
+wxlijetpfodamyiqpuzcrhstkg
+wxlnjzvbfidamyiqpuzcrbstkg
+wxlnjevefodavyiqpuzcthstkg
+wxlnjevbfozamyiqpurcrbstkg
+wxlnjfvpfodamyiqpuzcrhntkg
+wxlnjevbfvdamyiqvuzcrhqtkg
+wilejevbfodamyilpuzcrhstkg
+wxlnhevbfodamtiqpuzcrhstke
+wxlwjevbfodahyiqppzcrhstkg
+wxlnjevbfodamyuqpuzwrrstkg
+xxsnjevbfodamyiqpuzcrhstkl
+wglnjevbdodamyaqpuzcrhstkg
+wxlnjefbwodamyiqpuzcrhsykg
+wxwnjevbfodamyiqpuzcrhpckg
+wxlnjuvbfidamyiqpuzczhstkg
+wxlnzhybfodamyiqpuzcrhstkg
+wxunjevufodamyiqpuzcrhspkg
+wxunjevbfodcmyiqpuzcrhstcg
+wxlnjevbfodhwyiqpuxcrhstkg
+wxlnjevtfodasyiqpuzcrhstkb
+wxlvjevbfqdamyiqprzcrhstkg
+sxlnjevbfodamyiqplzcrhstkq
+wxlnlevbfodamyiqpuzcrpstka
+wxlnjevbfodaiyiqovzcrhstkg
+wxlntevbfodamyiqpuzcrkntkg
+wxlnjevbfodsmyiqpuzcrhstir
+wxlnnevbfodaoyiqpuzmrhstkg
+xxlnjevbfodamyinpnzcrhstkg
+wxlnjedefodamyigpuzcrhstkg
+wxlnxeabfodamyiqpnzcrhstkg
+wxlnxevbfodpmtiqpuzcrhstkg
+wxlnjevnfodamyiqpuzcuhqtkg
+wxlnjevbfodakyiqluzcrhstmg
+wxlnjevbaodamoiqpyzcrhstkg
+wwlnjevbfoaajyiqpuzcrhstkg
+wxlnjevbfedamyiqpuzcrhsang
+wxlwjevbfodamyiqpuzcrmdtkg
+wxlnjevbhodamyiqpmzxrhstkg
+wxlnjevbfodamyiqpuzzrhwtkj
+wxlnjevbfpdvmyiqpuzzrhstkg
+wxlnjegcfodamyiqpxzcrhstkg
+fxlnjevbfodamyiqpuzcrhstat
+wxlnjevbfodcmybqpuzcrkstkg
+wxlnjevbfodamiiqpuzrrhstxg
+wxvnjevifodamdiqpuzcrhstkg
+wxltjevbfodamyiqpuzmrhktkg
+wxlnjevbfobaaygqpuzcrhstkg
+wmlnjevbfodamyiqpuycrhsukg
+wxlnjevboodamyiqpuzcrhuhkg
+wxlnjevgfodaqyiqpuzcghstkg
+wxlnjevjnodamyiqpuzcrhstke
+wclnjevbfodamyiqpuncchstkg
+wxlnjevbfndamyxqpuzcshstkg
+rxldjevbfodamyiqpuvcrhstkg
+wxlnwevbfodamywqpuzrrhstkg
+ixlnjevbqodpmyiqpuzcrhstkg
+wxlnjlvbfocamyiqpuzgrhstkg
+wxlnjevffodamyiqnuzcrhutkg
+wxlajevbfodamyiqpuccrhshkg
+vwlnjevbfodamyiqpuzcrhstky
+wxlajevbfodamyiqpuzcfhstkl
+wxlnjevbfodamniqouzcrhstko
+wxlnjevbfodamyiqpuzqrhsqka
+wxlnjeybfodamyiqpuzclhsnkg
+wxbnjlvbfoyamyiqpuzcrhstkg
+wxbnjevbfodemyiqpuzcrhstkj
+wxlnbefbfodamyiqpkzcrhstkg
+wxlnjvvbyodamyitpuzcrhstkg
+jxlnjevbfopamyiqpuzprhstkg
+wxlnjevbfodaeyiupxzcrhstkg
+wnlnjevbfodamyqqpuzcrhstcg
+wxlxzuvbfodamyiqpuzcrhstkg
+wxlnjevbcodymyiqpuzcrhstke
+wxlnjezbfodamynqpuvcrhstkg
+wxlnjevbfodamyxlpuzcyhstkg
+wxlnjevbffdaiyiqpuzirhstkg
+wxlnjevbfodymyiqwuzcrhstfg
+wxlnzevbfodscyiqpuzcrhstkg
+hxlnjevbfodamyawpuzcrhstkg
+welnjevbfodamciqplzcrhstkg
+wxlnjeqbfodawyiqpuzkrhstkg
+wxlnjelbfodamviqpuzckhstkg
+wxlneevjfodamyiqpuzcrhstkd
+wxlnjevbfodamyaqpuytrhstkg
+wxlnjpvyfodamyiqpuzcshstkg
+wxlnjevbfodmbyiqpuzcrhstkp
+wxlnjegbfodamdiqpuzcrhstdg
+wmlnjevbfodamyiqpuecrhsukg
+wxlnjevbfodamyiqpuocrhwtjg
+jxfnwevbfodamyiqpuzcrhstkg
+wxlnjevffodamyiqpurcrhstkd
+wxlnjevbfofamyiqpuzcrhsmkt
+wxlnjevbfodmmyiqpuzcrdsttg
+axlnjevbfodgmyiqpuzerhstkg
+wxtnjevbfodamyifpuwcrhstkg
+wxlgjevbfodamyiqpuzrrhvtkg
+wxlnjevbfouamyeqfuzcrhstkg
+wxlnjevbfmxamyiqpuzcahstkg
+wxlnjevffoxamyiqpuecrhstkg
+wxlnyevbfodamyiqttzcrhstkg
+bxlnjevbfodzmysqpuzcrhstkg
+wxlnjevbfodayyiqpuzcrherkg
+yxlnjevbfodayyiqpuzcrwstkg
+wllnjevbfodambiqpuzurhstkg
+wxlnjevbfsdamyiqpuqcrhstkh
+wxlcjevbfodamyiqcuzcxhstkg
+wxlnjevbfodayticpuzcrhstkg
+wxltzevbfodamyiqpzzcrhstkg
+wxlnjevbfodamgiqpuzcphstng
+wxlnjevbfqdfmziqpuzcrhstkg
+wxlnaevbfodamyiqpuzcfustkg
+wxlnjevbfodamyxqxuzcrhstdg
+wxlnjevkbodamyiqpufcrhstkg
+whlnjevbfodauyiqputcrhstkg
+wxlnjevbfodamyiephzcrhsnkg
+wxlnjevbfodfmoiqpuzcrhstkf
+wxlnjevbfodamyiqxuzaxhstkg
+wxlnjevtfotamyiqpuzcrhsttg
+wxlgjevbfodamyiqpuhcrostkg
+dxlnjtvbfodamyiqpuzcshstkg
+wxlfjevbfodumyiqppzcrhstkg
+wxlnzevbfodamyiqpuzqrhstkx
+wflnjevbfodamyiqpurcrhsthg
+wxlnjevbfodzfyiqpuzcrjstkg
+wxlnjevbfrdamviqpuzmrhstkg
+wnlngevmfodamyiqpuzcrhstkg
+walzjevbfodamyiqpuzcrhsjkg
+wqlnjevbfodamyiqpuzcshslkg
+wxlnjevkfodfmyiepuzcrhstkg
+wxgnjehbfodamyhqpuzcrhstkg
+wxlnjevbfodamyiqfuacrostkg
+wxlnjexbfodamyiwpuzcrqstkg
+wxlntevafodamyiqpuzcrhsnkg
+wxvnjevbfodamyiqpuzcvistkg
+mxlnjeebfodamyiqpuzcrhsgkg
+wxlnjevyfodamyiqpuzcrhdtkf
+wxlnjcvbfodamyicpuzcrhsckg
+wxlnjekbfodlmyiqpuzcthstkg
+wxlnjvvbfodamyiopuzcrhstqg
+wxlnjevbsodamyiqpuhcrhstwg
+wxxnjevufodamyiqruzcrhstkg`
+
+const input2 = `abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`
+let twos = 0
+let threes = 0
+input.split('\n').forEach(inp => {
+  const dups = {}
+  inp.split('').forEach(i => {
+    if (dups[i] === undefined) {
+      dups[i] = 1
+    } else {
+      dups[i] += 1
+    }
+  })
+  new Set(Object.values(dups)).forEach(d => {
+    if (d === 2) {
+      twos += 1
+    }
+    if (d === 3) {
+      threes += 1
+    }
+  })
+})
+console.log(twos * threes)
+
+input.split('\n').forEach((inp, i) => {
+  input.split('\n').slice(i + 1).forEach(inp2 => {
+    const count = inp.split('').reduce((ans, inpa, idx) => {
+      if (inpa === inp2[idx]) {
+        return ans + 1
+      }
+      return ans
+    }, 0)
+
+    if (count === inp.length - 1) {
+      console.log(inp)
+      console.log(inp2)
+    }
+  })
+})
